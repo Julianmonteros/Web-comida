89394e486a0b9f2140de5ad2f3e3ce10271b4b6e
Julianmonteros <franciscomonteros1990@gmail.com>
2023-04-09 20:23:08 -0500

    navbar y busqueda

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('activ');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('activ');
+}
+
+
+document.querySelector('#search-icon').onclick = () => {
+    document.querySelector('#search-form').classList.toggle('action');
+}
+
+document.querySelector('#close').onclick = () => {
+    document.querySelector('#search-form').classList.remove('action');
+}
